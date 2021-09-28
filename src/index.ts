3c88e38de66d4119a33b2d9ebb67a10c2ca338d2
ItziarEspino <itziaespino19@iespuntadelverde.es>
2021-09-28 09:54:12 +0200

    primer commit

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { leerTeclado } from "./util/entradaTeclado";
 console.log("Hola")
 
 let main = async () => {
+    let edad: number
     const valor = await leerTeclado('¿Cuál es tu edad?')
-    let valor2 = parseInt (valor)
+    edad = parseInt (valor)
     console.log(`Tienes ${valor} años`)
-    if (valor2 < 18) {
+    if (edad < 18) {
         console.log("Eres menor de edad")
     } else {
         console.log("Eres mayor de edad")
